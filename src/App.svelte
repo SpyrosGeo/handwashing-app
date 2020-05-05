f7dbe85492597b9ea5b95d76ba313c8c79cb508f
SpyrosGeo <xarodasimba@gmail.com>
2020-05-05 20:19:38 +0300

    added components and basic styling

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,28 @@
+<script>
+  import HowTo from "./HowTo.svelte";
+  import Timer from "./Timer.svelte";
+</script>
 
+<style>
+  h1, h3 {
+    text-align: center;
+  }
+  a {
+    text-decoration: none;
+    color: black;
+  }
+  a:hover {
+    color: green;
+    transition: ease-in 0.2s;
+  }
+</style>
 
-<h1>Test</h1>
+<h1>HandWashing App</h1>
+<Timer />
+<HowTo />
 
-<p>fuck</p>
+<h3>
+  <a href="https://www.who.int/gpsc/clean_hands_protection/en/">
+    Picture Source
+  </a>
+</h3>
---
+++ src/Timer.svelte
@@ -0,0 +1,35 @@
+<script>
+import ProgressBar from './ProgressBar.svelte'
+
+</script>
+
+<style>
+h2{
+    margin: 0;
+}
+.start{
+    background-color: rgb(96, 139, 146);
+      width:100%;
+    margin:10px 0;
+    border: none;
+	color: #fff;
+	padding: 15px 32px;
+	text-align: center;
+	text-decoration: none;
+	display: inline-block;
+	font-size: 16px;
+	outline: none;
+	flex: 1;
+	cursor: pointer;
+}
+
+</style>
+
+<div bp="grid">
+<h2 bp="offset-5@md 4@md 12@sm" >Seconds Left:</h2>
+</div>
+<ProgressBar/>
+
+<button class="start">
+Start
+</button>
