33f334bf7f483340e69729d3f161f68236736dc0
SpyrosGeo <xarodasimba@gmail.com>
2020-05-05 19:25:41 +0300

    initialized app

---
+++ src/App.svelte
@@ -0,0 +1,24 @@
+<script>
+	export let name;
+</script>
+
+<main>
+	<h1>Hello {name}!</h1>
+	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+</main>
+
+<style>
+	main {
+	
+	}
+
+	h1 {
+		
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
