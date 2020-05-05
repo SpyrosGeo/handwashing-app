078ccd550484eebab8ab27f5fe228d8049e72a97
SpyrosGeo <xarodasimba@gmail.com>
2020-05-05 19:39:53 +0300

    got rollup config working

--- src/App.svelte
+++ src/App.svelte
@@ -1,24 +1,5 @@
-<script>
-	export let name;
-</script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
 
-<style>
-	main {
-	
-	}
+<h1>Test</h1>
 
-	h1 {
-		
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
+<p>fuck</p>
